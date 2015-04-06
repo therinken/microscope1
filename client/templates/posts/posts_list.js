1534baa7aa130e6f5ecebaa027e2b49024bbc12d
Ken McLeod <admin@therinken.com>
2015-04-05 21:59:51 -0500

    sorting and submit logic

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,9 @@
 Template.postsList.helpers({
-    posts: function(){
-    return Posts.find();
+    posts: function() {
+        return Posts.find({}, {
+            sort: {
+                submitted: -1
+            }
+        });
     }
 });
